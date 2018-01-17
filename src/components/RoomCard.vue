1267075089c62bf1c23fcded88f7bca4646afd0a
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-01-17 11:24:56 +0100

    Add a RoomCard component

---
+++ src/components/RoomCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <md-card>
+    <md-card-header class="md-title">Room 1</md-card-header>
+    <md-card-content>
+      <md-list>
+        <md-list-item>
+          <md-icon>lock_outline</md-icon>
+          <span class="md-list-item-text">Door is locked.</span>
+        </md-list-item>
+      </md-list>
+    </md-card-content>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'room-card'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/SensorOverview.vue
+++ src/components/SensorOverview.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <h1 class="md-headline">Sensor Overview</h1>
+    <RoomCard/>
   </div>
 </template>
 
 <script>
+import RoomCard from './RoomCard'
+
 export default {
-  name: 'sensor-overview'
+  name: 'sensor-overview',
+  components: {
+    RoomCard
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
