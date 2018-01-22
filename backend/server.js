036270ddee528dfa14d44362e17230df10848eb5
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-01-22 13:38:15 +0100

    Reduce templating to simple html file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/lock', function (req, res) {
