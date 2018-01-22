16cbda636d20628c7a7b38d0c5b5f9ef841531de
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-01-22 13:57:21 +0100

    Add ReST endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,29 @@ const io = require('socket.io')(http)
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+let data = {
+    is_locked: false
+}
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/status', function(req, res) {
+    res.json(data)
+})
+
 app.get('/lock', function (req, res) {
-    io.emit('lock-update', true)
+    io.emit('lock-update', data.is_locked)
     res.send('Locked.')
 })
 app.get('/unlock', function (req, res) {
-    io.emit('lock-update', false)
+    io.emit('lock-update', data.is_locked)
     res.send('Unlocked.')
 })
 
 io.on('connection', function(socket) {
-    console.log("New connection")
-    io.emit('lock-update', true)
+    socket.emit('lock-update', data.is_locked)
 })
 
 http.listen(3000, function () {
